421f2a33d781b281ee7bb0473c946ed8641fbe44
Yash Agrawal <yashagrawal0916@gmail.com>
2024-06-04 20:49:58 +0530

    Revisited the basics of React

---
+++ tutorial/src/App.jsx
@@ -0,0 +1,7 @@
+import Chai from "./Chai"
+export default function App (){
+    return <>
+        <h1>Hello World</h1>
+        <Chai />
+    </>
+};
---
+++ tutorial/src/Chai.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Chai(){
+    return <h1>Chai aur React</h1>
+}
