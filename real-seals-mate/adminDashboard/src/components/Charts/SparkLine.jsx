864076fdb9488e9379fa38c59729d6f0b166de03
Yash Agrawal <yashagrawal0916@gmail.com>
2024-06-25 20:43:10 +0530

    eCommerce page complete

--- real-seals-mate/adminDashboard/src/components/Charts/SparkLine.jsx
+++ real-seals-mate/adminDashboard/src/components/Charts/SparkLine.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {SparklineComponent ,Inject , SparklineTooltip} from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
+const SparkLine = ({id,height,width,color,currentColor,type,data}) => {
   return (
-    <div>SparkLine</div>
+    <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        tooltipSettings={{
+          visible: true,
+
+          format: '${x} : data ${yval}',
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+      >
+
+    <Inject services={[SparklineTooltip]} />
+
+    </SparklineComponent>
   )
 }
 
--- real-seals-mate/adminDashboard/src/pages/Ecommerce.jsx
+++ real-seals-mate/adminDashboard/src/pages/Ecommerce.jsx
@@ -76,8 +76,28 @@ const Ecommerce = () => {
                 <p className='text-gray-500 mt-1'>Expense</p>
               </div>
               <div className='mt-5'>
-                <SparkLine/>
+                <SparkLine
+                  currentColor = "blue"
+                  id = "line-sparkline"
+                  type = "Line"
+                  height = "80"
+                  width = "250"
+                  data = {SparklineAreaData}
+                  color = "blue"
+                />
               </div>
+              <div className='mt-10'>
+                <Button 
+                  color="white"
+                  bgColor="blue"
+                  text= "Download Report"
+                  borderRadius="10px"
+
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked width ="320px"  height="360px"/>
             </div>
           </div>
         </div>
