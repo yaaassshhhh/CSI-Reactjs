3e9a200ada22e7188ecaf1a4fbdd43db9818f654
Yash Agrawal <yashagrawal0916@gmail.com>
2024-06-26 22:14:28 +0530

    kanban-board,editor and Calendar complete

--- real-seals-mate/adminDashboard/src/pages/Editor.jsx
+++ real-seals-mate/adminDashboard/src/pages/Editor.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {HtmlEditor ,  Image , Inject,Link,QuickToolbar,RichTextEditorComponent,Toolbar} from "@syncfusion/ej2-react-richtexteditor"
 const Editor = () => {
   return (
     <div>Editor</div>
